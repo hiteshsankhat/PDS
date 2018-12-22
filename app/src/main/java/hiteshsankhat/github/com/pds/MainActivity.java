73089540ec5ccd8a420d22065c784ba3acc9de5e
Hitesh Sankhat <hsankhat099@gmail.com>
2018-12-22 21:13:25 +0530

    Google Map Api SetUp

--- app/src/main/java/hiteshsankhat/github/com/pds/MainActivity.java
+++ app/src/main/java/hiteshsankhat/github/com/pds/MainActivity.java
@@ -1,13 +1,58 @@
 package hiteshsankhat.github.com.pds;
 
+import android.app.Dialog;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    private static final int ERROE_DIALOG_REQUEST = 9001;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if(isServicesOk()){
+            init();
+        }
+    }
+
+    private void init(){
+        Button btnMap = findViewById(R.id.btn_map);
+        btnMap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, MapActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+    public boolean isServicesOk(){
+        Log.d(TAG, "isServicesOk: google services");
+
+        int available = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(MainActivity.this);
+
+        if (available == ConnectionResult.SUCCESS){
+            Log.d(TAG, "isServicesOk: google play services is wroking");
+            return  true;
+        }else if(GoogleApiAvailability.getInstance().isUserResolvableError(available)){
+            Log.d(TAG, "isServicesOk: repairing the error");
+            Dialog dialog = GoogleApiAvailability.getInstance().getErrorDialog(MainActivity.this, available, ERROE_DIALOG_REQUEST);
+            dialog.show();
+        }else{
+            Toast.makeText(this, "You Can't Use Map....", Toast.LENGTH_SHORT).show();
+        }
+        return  false;
     }
 }
---
+++ app/src/main/java/hiteshsankhat/github/com/pds/MapActivity.java
@@ -0,0 +1,15 @@
+package hiteshsankhat.github.com.pds;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MapActivity extends AppCompatActivity {
+
+    private static final String TAG = "MapActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+    }
+}
