115c95f34d9e7621035ae8a5ded8f6b9d27bc44c
Hitesh Sankhat <hsankhat099@gmail.com>
2018-12-22 23:41:06 +0530

    Map Permissions and Callback

--- app/src/main/java/hiteshsankhat/github/com/pds/MapActivity.java
+++ app/src/main/java/hiteshsankhat/github/com/pds/MapActivity.java
@@ -1,15 +1,90 @@
 package hiteshsankhat.github.com.pds;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
-public class MapActivity extends AppCompatActivity {
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+
+public class MapActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     private static final String TAG = "MapActivity";
+
+    private  static  final String FINE_LOCTION = Manifest.permission.ACCESS_FINE_LOCATION;
+    private  static  final String COURSE_LOCTION = Manifest.permission.ACCESS_COARSE_LOCATION;
+    private  static  final int LOCATION_PERMISSION_REQUEST_CODE = 1234;
+
+
+    //variables
+    private  boolean mLocationPermissionsGranted = false;
+    private GoogleMap mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+
+        getLocationPermission();
+    }
+
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        Log.d(TAG, "onRequestPermissionsResult: Called");
+        mLocationPermissionsGranted = false;
+
+        switch (requestCode){
+            case LOCATION_PERMISSION_REQUEST_CODE:{
+                if (grantResults.length > 0 ){
+                    for (int i = 0 ; i < grantResults.length; i++){
+                        if (grantResults[i] != PackageManager.PERMISSION_GRANTED){
+                            mLocationPermissionsGranted = false;
+                            return;
+                        }
+                    }
+                    mLocationPermissionsGranted = true;
+                    initMap();
+                }
+            }
+        }
+    }
+
+
+    private  void initMap(){
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+
+        Log.d(TAG, "initMap: init");
+        mapFragment.getMapAsync(MapActivity.this);
+    }
+
+    private void getLocationPermission(){
+        Log.d(TAG, "getLocationPermission: Permission");
+        String [] permissions = {FINE_LOCTION, COURSE_LOCTION};
+
+        if (ContextCompat.checkSelfPermission(this.getApplicationContext(), FINE_LOCTION) == PackageManager.PERMISSION_GRANTED){
+            if (ContextCompat.checkSelfPermission(this.getApplicationContext(), COURSE_LOCTION) == PackageManager.PERMISSION_GRANTED) {
+                mLocationPermissionsGranted = true;
+                Log.d(TAG, "getLocationPermission: yes");
+            } else{
+                ActivityCompat.requestPermissions(this, permissions, LOCATION_PERMISSION_REQUEST_CODE);
+            }
+        } else{
+            ActivityCompat.requestPermissions(this, permissions, LOCATION_PERMISSION_REQUEST_CODE);
+        }
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        Toast.makeText(this, "Map is Ready", Toast.LENGTH_SHORT).show();
+        Log.d(TAG, "onMapReady: Map is ready here");
+        mMap = googleMap;
     }
 }
